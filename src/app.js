2890b549dbc26fce46b57dc779350cd7fa1851dc
Ri-1906 <riddhi030304@gmail.com>
2026-09-22 23:05:50 +0530

    added acount router

--- src/app.js
+++ src/app.js
@@ -1,15 +1,22 @@
 const express = require('express')
-const authRouter = require('./routes/auth.routes')
-
 const cookieParser = require('cookie-parser')
 
 const app = express()
 
 app.use(express.json())
-app.use(cookieParser)
+app.use(cookieParser())
 
+/**
+ * - Routes
+ */
+const authRouter = require('./routes/auth.routes')
+const accountRouter = require('./routes/account.route')
+
+/**
+ * - Use Routes
+ */
 
 app.use("/api/auth", authRouter)
-
+app.use('/api/accounts',accountRouter)
 
 module.exports = app
